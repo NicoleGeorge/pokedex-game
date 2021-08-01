0acfc27386a65c6bbd26783ed3db0ee69ce0f5a2
Nicole George <hello@nicolegeorge.me>
2021-08-01 18:19:57 +0930

    winner and loser decks rendering conditionally

--- src/Pokegame.js
+++ src/Pokegame.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import Pokedex from './Pokedex'
 
 class Pokegame extends Component {
     static defaultProps = {
@@ -21,11 +22,12 @@ class Pokegame extends Component {
             let randPokemon = hand2.splice(randIndex, 1)[0];
             hand1.push(randPokemon);
         }
-        console.log(hand1)
-        console.log(hand2)
+        let exp1 = hand1.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
+        let exp2 = hand2.reduce((exp, pokemon) => exp + pokemon.base_experience, 0);
         return (
             <div>
-                <h1>POKEGAME!</h1>
+                <Pokedex pokemon={hand1} exp={exp1} isWinner={exp1 > exp2} />
+                <Pokedex pokemon={hand2} exp={exp2} isWinner={exp2 > exp1} />
             </div>
         )
     }
