d2f7f384ae21dd54b2704f524d26d3ba5de66594
Nicole George <hello@nicolegeorge.me>
2021-08-01 17:53:01 +0930

    pokemon cards rendering, light styling applied

--- src/Pokecard.js
+++ src/Pokecard.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import './Pokecard.css'
+
 const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
 
 class Pokecard extends Component {
@@ -6,8 +8,10 @@ class Pokecard extends Component {
         let imgSrc = `${POKE_API}${this.props.id}.png`
         return (
             <div className="Pokecard">
-                <h1>{this.props.name}</h1>
-                <img src={imgSrc} />
+                <h1 className="Pokecard-title">{this.props.name}</h1>
+                <img src={imgSrc} alt={this.props.name}/>
+                <div className="Pokecard-data">TYPE: {this.props.type}</div>
+                <div className="Pokecard-data">EXPERIENCE: {this.props.exp}</div>
             </div>
         );
     }
